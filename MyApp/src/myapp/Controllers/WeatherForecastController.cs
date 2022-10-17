0a5b0f8df5d15ecceb64775bfb7a03d7f1726c58
Gunnar L. Rasmussen <gunnarlr@outlook.com>
2022-10-17 09:02:37 +0000

    Changed get and post to dapr versions

--- MyApp/src/myapp/Controllers/WeatherForecastController.cs
+++ MyApp/src/myapp/Controllers/WeatherForecastController.cs
@@ -21,4 +21,13 @@ public class WeatherForecastController : ControllerBase
         // no matter where the state is stored, go get it for me
         return await daprClient.GetStateAsync<WeatherForecast>("statestore", location);
     }
+
+    [HttpPost]
+    [Dapr.Topic("pubsub","new")] // When something happens on topic "new" on pubsub called "pubsub", call me
+    public async Task<ActionResult<WeatherForecast>> PostWeatherForecast(WeatherForecast model, [FromServices] Dapr.Client.DaprClient daprClient)
+    {
+        await daprClient.SaveStateAsync<WeatherForecast>("statestore", model.Location, model);
+
+        return model;
+    }
 }
